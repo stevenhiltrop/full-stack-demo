53b3bee4058469bfee23558ebde3501769a817fc
stevenhiltrop <stevenhiltrop@gmail.com>
2021-08-15 12:27:30 +0700

    Fixed typing errors

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -12,5 +12,5 @@ urlpatterns=[
     url(r'^employee/$', views.employeeApi),
     url(r'^employee/([0-9]+)$', views.employeeApi),
 
-    url(r'^SaveFile$', views.views.SaveFile)
+    url(r'^SaveFile$', views.SaveFile)
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
